37761634ad9cfc44f988b3d30c6033b77e866192
marco-bravo-tsoft <marco.bravo@tsoftlatam.com>
2017-06-08 12:44:22 -0400

    Agregado Webpack para desarrollo de front end

---
+++ front-end/app/index.js
@@ -0,0 +1,11 @@
+import _ from 'lodash';
+
+function component () {
+	var element = document.createElement("div");
+
+	element.innerHTML = _.join(["Bienvenido","inspector"], " ");
+
+	return element;
+}
+
+document.body.appendChild(component());
